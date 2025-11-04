b1f7abff294626274a829879576e54deb4aba282
Dougbsf <dd44op@gmail.com>
2025-11-04 19:37:10 -0300

    feat: added swiper

--- src/app/components/online-personal-trainer-list/online-personal-trainer-list.component.ts
+++ src/app/components/online-personal-trainer-list/online-personal-trainer-list.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
 
+register();
 @Component({
   selector: 'app-online-personal-trainer-list',
   templateUrl: './online-personal-trainer-list.component.html',
   styleUrls: ['./online-personal-trainer-list.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class OnlinePersonalTrainerListComponent  implements OnInit {
+export class OnlinePersonalTrainerListComponent {
+ spaceBetween = 10;
 
-  constructor() { }
+  onProgress(event: CustomEvent<[Swiper, number]>) {
+    const [swiper, progress] = event.detail;
+    console.log(progress);
+  }
 
-  ngOnInit() {}
+  onSlideChange() {
+    console.log('slide changed');
+  }
 
 }
--- src/app/components/training-programs-list/training-programs-list.component.ts
+++ src/app/components/training-programs-list/training-programs-list.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
 
 @Component({
   selector: 'app-training-programs-list',
   templateUrl: './training-programs-list.component.html',
   styleUrls: ['./training-programs-list.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class TrainingProgramsListComponent  implements OnInit {
+export class TrainingProgramsListComponent {
 
-  constructor() { }
+  onProgress(event: CustomEvent<[Swiper, number]>) {
+    const [swiper, progress] = event.detail;
+    console.log(progress);
+  }
 
-  ngOnInit() {}
+  onSlideChange() {
+    console.log('slide changed');
+  }
 
 }
